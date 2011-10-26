5840a5e0d76e4a927d91df64f698a3a4d19b5b5a
revtr <revtr@073d4cb9-f9b1-4ef5-a5a0-1f7977fa763d>
2011-10-26 01:17:59 +0000

    Adding another first level filter for poisoniers
    git-svn-id: svn+ssh://slider.cs.washington.edu/homes/network/revtr/ugrad_svn/spoofed_traceroute@666 073d4cb9-f9b1-4ef5-a5a0-1f7977fa763d

--- filters.rb
+++ filters.rb
@@ -9,7 +9,8 @@ module FirstLevelFilters
     NO_VP_RECENTLY_OBSERVING = :no_vp_recently_observing
     NO_STABLE_UNCONNECTED_VP = :no_stable_unconnected_vp
     NO_STABLE_CONNECTED_VP = :no_stable_connected_vp
-    NO_NON_POISONER = :no_non_poisoner
+    NO_NON_POISONER_OBSERVING = :no_non_poisoner
+    NO_NON_POISONER_CONNECTED = :no_non_poisoner_connected
     NO_VP_REMAINS = :no_vp_remains
     ALL_NODES_ISSUED_MEASUREMENTS_RECENTLY = :all_nodes_issued_measurements_recently
 
@@ -31,8 +32,12 @@ module FirstLevelFilters
             filter_tracker.failure_reasons << NO_STABLE_CONNECTED_VP 
         end
 
-        if self.no_non_poisoner?(nodes)
-            filter_tracker.failure_reasons << NO_NON_POISONER
+        if self.no_non_poisoner_observing?(nodes)
+            filter_tracker.failure_reasons << NO_NON_POISONER_OBSERVING
+        end
+
+        if self.no_non_poisoner_connected?(stillconnected)
+            filter_tracker.failure_reasons << NO_NON_POISONER_CONNECTED
         end
 
         if self.no_vp_remains?(observingnode2rounds)
@@ -62,10 +67,12 @@ module FirstLevelFilters
        stillconnected.find { |node| (now - (nodetarget2lastoutage[[node, target]] or Time.at(0))) / 60 > LOWER_ROUNDS_BOUND }.nil?
     end
 
-    # (if a poisoner is observing, make sure at least on other non-poinonser is also observing)
-    def self.no_non_poisoner?(nodes)
-       # if at least one poisoner, make sure at least one non-poisoner
-       (nodes.find { |n| FailureIsolation::PoisonerNames.include? n }) ? nodes.find { |n| !FailureIsolation::PoisonerNames.include? n } : false
+    def self.no_non_poisoner_observing?(nodes)
+       nodes.find { |n| !FailureIsolation::PoisonerNames.include? n }
+    end
+
+    def self.no_non_poisoner_connected?(stillconnected)
+       stillconnected.find { |n| !FailureIsolation::PoisonerNames.include? n } 
     end
 
     # (at least one observing node remains)
