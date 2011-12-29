e18a4427b499f5586ae6b9faa883ca6dc2831622
revtr <revtr@073d4cb9-f9b1-4ef5-a5a0-1f7977fa763d>
2011-12-29 21:23:27 +0000

    Mail.rb wasn't rendering views properly?
    git-svn-id: svn+ssh://slider.cs.washington.edu/homes/network/revtr/ugrad_svn/spoofed_traceroute@738 073d4cb9-f9b1-4ef5-a5a0-1f7977fa763d

--- mail.rb
+++ mail.rb
@@ -10,46 +10,50 @@ require 'action_mailer'
 
 ActionMailer::Base.delivery_method = :sendmail
 #ActionMailer::Base.template_root = "#{$REV_TR_TOOL_DIR}/templates"
-ActionMailer::Base.prepend_view_path("#{$REV_TR_TOOL_DIR}/templates")
+ActionMailer::Base.prepend_view_path("./templates")
 #$TEMPLATE_PATH = "#{$REV_TR_TOOL_DIR}/templates"
 ActionMailer::Base.raise_delivery_errors = true
+
+puts ActionMailer::Base.view_paths
 
 class Emailer < ActionMailer::Base
     LOGGER = LoggerLog.new($stderr)
 
     def test_email(email)
-        mail :subject => "Ashoat is testing!", 
+        mail(:subject => "Ashoat is testing!", 
              :from => "revtr@cs.washington.edu",
-             :recipients => email
+             :recipients => email)
     end
     def successful_revtr(email, source, destination, traceroute)
         @source = source
         @destination = destination
         @traceroute = traceroute
 
-        mail :subject => "Reverse traceroute from #{destination} back to #{source}",
+        mail(:subject => "Reverse traceroute from #{destination} back to #{source}",
              :from => "revtr@cs.washington.edu",
              :recipients => email,
-             :bcc => "revtr@cs.washington.edu"
+             :bcc => "revtr@cs.washington.edu")
     end
     def timeout_error(email)
-        mail :subject => "[revtr error] Execution of measurement exceeded timeout",
+        mail(:subject => "[revtr error] Execution of measurement exceeded timeout",
              :from => "revtr@cs.washington.edu",
              :recipients => email,
-             :bcc => "revtr@cs.washington.edu"
+             :bcc => "revtr@cs.washington.edu")
     end
     def timeout_to_us(emails)
         @emails = emails
 
-        mail :subject => "[revtr error] Execution of measurement exceeded timeout",
+        mail(:subject => "[revtr error] Execution of measurement exceeded timeout",
              :from => "revtr@cs.washington.edu",
-             :recipients => "revtr@cs.washington.edu"
+             :recipients => "revtr@cs.washington.edu")
     end
     def error(email)
-        mail :subject => "[revtr error] Error occurred!",
+        mail(:subject => "[revtr error] Error occurred!",
              :from => "revtr@cs.washington.edu",
              :recipients => email,
-             :bcc => "revtr@cs.washington.edu"
+             :bcc => "revtr@cs.washington.edu") do |format|
+            format.html { render "error.text.html.erb" } 
+        end
     end
     def critical_error(script, error, backtrace, emails)
         @script = script
@@ -57,9 +61,11 @@ class Emailer < ActionMailer::Base
         @bt = backtrace
         @emails = emails
 
-        mail :subject => "[revtr error] CRITICAL ERROR OCCURRED!",
+        mail(:subject => "[revtr error] CRITICAL ERROR OCCURRED!",
              :from => "revtr@cs.washington.edu",
-             :recipients => "revtr@cs.washington.edu"
+             :recipients => "revtr@cs.washington.edu") do |format|
+            format.html { render "critical_error.text.html.erb" } 
+        end
     end
     def tr_parse_fail(source, destination, error, tr, log_id, email)
         @error = error
@@ -67,9 +73,11 @@ class Emailer < ActionMailer::Base
         @log_id = log_id
         @email = email
 
-        mail :subject => "Could not parse given traceroute from #{destination} back to #{source}",
+        mail(:subject => "Could not parse given traceroute from #{destination} back to #{source}",
              :from => "revtr@cs.washington.edu",
-             :recipients => "revtr@cs.washington.edu"
+             :recipients => "revtr@cs.washington.edu") do |format|
+            format.html { render "tr_parse_fail.text.html.erb" } 
+        end
     end
     def tr_parse_success(source, destination, tr, revtr, parsed, log_id, email)
         @tr = tr
@@ -78,61 +86,75 @@ class Emailer < ActionMailer::Base
         @log_id = log_id
         @email = email
 
-        mail :subject => "Traceroute comparison results from #{destination} back to #{source}",
+        mail(:subject => "Traceroute comparison results from #{destination} back to #{source}",
              :from => "revtr@cs.washington.edu",
-             :recipients => "revtr@cs.washington.edu"
+             :recipients => "revtr@cs.washington.edu") do |format|
+            format.html { render "tr_parse_success.text.html.erb" } 
+        end
     end
     def tr_fail(email, source, destination)
         @source = source
         @dest = destination
 
-        mail :subject => "Reverse traceroute from #{destination} back to #{source}",
+        mail(:subject => "Reverse traceroute from #{destination} back to #{source}",
              :from => "revtr@cs.washington.edu",
              :recipients => email,
-             :bcc => "revtr@cs.washington.edu"
+             :bcc => "revtr@cs.washington.edu") do |format|
+            format.html { render "tr_fail.text.html.erb" } 
+        end
     end
     def revtr_fail(email, source, destination)
         @source = source
         @dest = destination
 
-        mail :subject => "Reverse traceroute from #{destination} back to #{source}",
+        mail(:subject => "Reverse traceroute from #{destination} back to #{source}",
              :from => "revtr@cs.washington.edu",
              :recipients => email,
-             :bcc => "revtr@cs.washington.edu"
+             :bcc => "revtr@cs.washington.edu") do |format|
+            format.html { render "revtr_fail.text.html.erb" } 
+        end
     end
     def blocked(email, source, destination)
         @dest = destination
 
-        mail :subject => "Reverse traceroute from #{destination} back to #{source}",
+        mail(:subject => "Reverse traceroute from #{destination} back to #{source}",
              :from => "revtr@cs.washington.edu",
              :recipients => email,
-             :bcc => "revtr@cs.washington.edu"
+             :bcc => "revtr@cs.washington.edu") do |format|
+            format.html { render "blocked.text.html.erb" } 
+        end
     end
     def check_up_vps_issue(vps, issue)
         @vps = vps.join("<br />")
         @issue = issue
 
-        mail :subject => "VPs having trouble with #{issue}",
+        mail(:subject => "VPs having trouble with #{issue}",
              :from => "revtr@cs.washington.edu",
-             :recipients => "revtr@cs.washington.edu"
+             :recipients => "revtr@cs.washington.edu") do |format|
+            format.html { render "check_up_vps_issue.text.html.erb" } 
+        end
     end
     def isolation_results(merged_outage)
         LOGGER.info "Attempted to send isolation_results email"
 
         @merged_outage = merged_outage
 
-        mail :subject => "Isolation: #{merged_outage.direction}; #{merged_outage.datasets.join ' '}; sources: #{merged_outage.sources.join ' '}",
+        mail(:subject => "Isolation: #{merged_outage.direction}; #{merged_outage.datasets.join ' '}; sources: #{merged_outage.sources.join ' '}",
              :from => "failures@cs.washington.edu",
-             :recipients => "failures@cs.washington.edu"
+             :recipients => "failures@cs.washington.edu") do |format|
+            format.html { render "isolation_results.text.html.erb" } 
+        end
     end
     def isolation_exception(exception, recipient="failures@cs.washington.edu")
         LOGGER.info "Attempted to send isolation_exception email"
 
         @exception = exception
 
-        mail :subject => "Isolation Module Exception",
+        mail(:subject => "Isolation Module Exception",
              :from => "failures@cs.washington.edu",
-             :recipients => recipient
+             :recipients => recipient) do |format|
+            format.html { render "isolation_exception.text.html.erb" } 
+        end
     end
     def faulty_node_report(outdated_nodes, problems_at_the_source, not_sshable, not_controllable, failed_measurements,
                           bad_srcs, possibly_bad_srcs)
@@ -146,9 +168,11 @@ class Emailer < ActionMailer::Base
         @bad_srcs = bad_srcs
         @possibly_bad_srcs = possibly_bad_src
 
-        mail :subject => "faulty monitoring node report",
+        mail(:subject => "faulty monitoring node report",
              :from => "failures@cs.washington.edu",
-             :recipients => "failures@cs.washington.edu"
+             :recipients => "failures@cs.washington.edu") do |format|
+            format.html { render "faulty_node_report.text.html.erb" } 
+        end
     end
     def isolation_status(dataset2unresponsive_targets, possibly_bad_targets, bad_hops, possibly_bad_hops)
         LOGGER.info "Attempted to send faulty_node_report email"
@@ -158,18 +182,22 @@ class Emailer < ActionMailer::Base
         @bad_hops = bad_hops
         @possibly_bad_hops = possibly_bad_hops
 
-        mail :subject => "Isolation target status",
+        mail(:subject => "Isolation target status",
              :from => "failures@cs.washington.edu",
-             :recipients => "failures@cs.washington.edu"
+             :recipients => "failures@cs.washington.edu") do |format|
+            format.html { render "isolation_status.text.html.erb" } 
+        end
     end
     def poison_notification(outage)
         LOGGER.info "Attempted to send poison_notification email"
 
         @outage = outage
         
-        mail :subject => "Poison Opportunity Detected!",
+        mail(:subject => "Poison Opportunity Detected!",
              :from => "failures@cs.washington.edu",
-             :recipients => "failures@cs.washington.edu"
+             :recipients => "failures@cs.washington.edu") do |format|
+            format.html { render "poison_notification.text.html.erb" } 
+        end
     end
 end
 
@@ -184,6 +212,5 @@ def email_and_die
 end
 
 if __FILE__ == $0
-    puts Emailer.isolation_exception("how did you get here").class
-    puts Emailer.isolation_status({},[],[],[])
+    puts Emailer.isolation_exception("ActionMailer is broken?", "ikneaddough@gmail.com")
 end
